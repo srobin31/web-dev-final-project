46560b9151c16102c5289ae3b1351f64c447f86c
Shawn Robin <robin.sh@northeastern.edu>
2022-04-28 13:03:28 -0400

    add util function for getting age from given date

--- frontend/src/utils/date.js
+++ frontend/src/utils/date.js
@@ -5,3 +5,14 @@ export const renderDateStr = (dateStr) => {
   const day = date.getDate().toString().padStart(2, "0");
   return `${month}/${day}/${year}`;
 };
+
+export const getAge = (birthday) => {
+  const today = new Date();
+  let age = today.getFullYear() - birthday.getFullYear();
+  const m = today.getMonth() - birthday.getMonth();
+  const d = today.getDate() - birthday.getDate();
+  if (m < 0 || (m === 0 && d < 0)) {
+      age -= 1;
+  }
+  return age;
+};
