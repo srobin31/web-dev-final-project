ed4fc81761453ddda22be0d00c33c76b73700763
Shawn Robin <robin.sh@northeastern.edu>
2022-05-01 18:11:23 -0400

    show users their own personal info, allow email changes

--- frontend/src/components/profile/index.js
+++ frontend/src/components/profile/index.js
@@ -3,9 +3,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { getProfile } from "../../actions/auth-actions";
-import { getUserById } from "../../services/users-service";
+import { getUserById, updateEmail } from "../../services/users-service";
 
-import { renderDateStr } from "../../utils/date";
+import { renderDateStr, getBirthday, getAge } from "../../utils/date";
 
 import Nav from "../home/nav";
 import ReviewedDrinks from "./reviews";
@@ -15,6 +15,9 @@ const Profile = () => {
 
   const [profile, setProfile] = useState(stateProfile);
   const [profileFetched, setProfileFetched] = useState(false);
+  const [showPersonalInfo, setShowPersonalInfo] = useState(true);
+  const [editEmail, setEditEmail] = useState(false);
+  const [newEmail, setNewEmail] = useState("");
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -35,10 +38,13 @@ const Profile = () => {
     } else {
       fetchProfile();
     }
-  }, [dispatch, params]);
+  }, [dispatch, params, ]);
 
   useEffect(() => {
     setProfile(stateProfile);
+    if (stateProfile) {
+      setNewEmail(stateProfile.email);
+    }
   }, [stateProfile, params]);
 
   useEffect(() => {
@@ -46,6 +52,117 @@ const Profile = () => {
       navigate("/login");
     }
   }, [profile, profileFetched, params, navigate]);
+
+  const error = (msg) => {
+    setEditEmail(false);
+    setNewEmail(profile.email);
+    alert(msg);
+  };
+
+  const handleEmailChange = () => {
+    if (newEmail === profile.email) {
+      setEditEmail(false);
+    } else if (newEmail === "") {
+      error("Email cannot be blank.");
+    } else {
+      const updatedEmail = {
+        userId: profile._id,
+        email: newEmail,
+      };
+      updateEmail(updatedEmail)
+        .then((res) => {
+          if (res.error) {
+            error(res.error);
+          } else {
+            setEditEmail(false);
+            setProfile({ ...profile, email: newEmail });
+          }
+        });
+    }
+  };
+
+  const renderEmail = () => {
+    if (!editEmail) {
+      return (
+        <>
+          {profile.email}
+          <i
+            type="button"
+            className="fas fa-pen float-end lh-base"
+            onClick={(e) => {
+              setEditEmail(!editEmail);
+            }}
+          ></i>
+        </>
+      );
+    } else {
+      return (
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control"
+            id="email"
+            value={newEmail}
+            onChange={(e) => {
+              setNewEmail(e.target.value);
+            }}
+          />
+          <div className="input-group-append">
+            <button className="btn btn-primary" type="button" onClick={handleEmailChange}>
+              Save
+            </button>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  const renderPersonalInfo = () => {
+    return (
+      <div className="row">
+        <div className="col-5">
+          <p>
+            <button
+              className="btn btn-info"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#accountInfo"
+              aria-expanded="false"
+              aria-controls="accountInfo"
+              onClick={(e) => {
+                if (!showPersonalInfo) {
+                  setEditEmail(false);
+                }
+                setShowPersonalInfo(!showPersonalInfo);
+              }}
+            >
+              {`${showPersonalInfo ? "Show" : "Hide"} Personal Information`}
+            </button>
+          </p>
+          <div className="collapse" id="accountInfo">
+            <div className="card card-body border-info">
+              <table className="table card-table mb-0  border-info">
+                <tbody>
+                  <tr>
+                    <td>Email</td>
+                    <td>{profile && renderEmail()}</td>
+                  </tr>
+                  <tr>
+                    <td>Birthday</td>
+                    <td>{profile && getBirthday(profile.dob)}</td>
+                  </tr>
+                  <tr>
+                    <td>Age</td>
+                    <td>{profile && getAge(profile.dob)}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <>
@@ -64,8 +181,9 @@ const Profile = () => {
               </h4>
             )}
           </div>
-          <hr />
         </div>
+        {params.uid ? <></> : renderPersonalInfo()}
+        <hr />
         {profile && <ReviewedDrinks profile={profile} />}
       </div>
     </>
